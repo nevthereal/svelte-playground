32c8054270d8cd9dc9f6c2adec2441f81b63f361
Neville Brem <contact@nevillebrem.com>
2023-11-09 08:18:41 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import ProductCard from '$lib/productCard.svelte';
+
+	const products: Product[] = [
+		{
+			id: '1',
+			name: 'coffee',
+			price: 5
+		},
+		{
+			id: '2',
+			name: 'sunglasses',
+			price: 10
+		},
+		{
+			id: '3',
+			name: 'bottle',
+			price: 15
+		}
+	];
+</script>
+
+<div class="m-16">
+	<div class="grid grid-cols-3 gap-4">
+		<div class="col-span-3">
+			<h1 class="text-3xl">Amazing Store</h1>
+		</div>
+		{#each products as product}
+			<ProductCard {product} />
+		{/each}
+		<div class="col-span-3">
+			<button class="btn variant-filled">Checkout with Stripe</button>
+		</div>
+	</div>
+</div>
---
+++ src/routes/api/stripeCheckout/+server.ts
@@ -0,0 +1,2 @@
+import type { RequestHandler } from './$types';
+import Stripe from 'stripe';
